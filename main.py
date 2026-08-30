730f7769890a40871ef561bc240fdfbb39bfeeff
VIG0002 <vig0002@mhs.vic.edu.au>
2026-08-31 09:07:33 +1000

    Implemented some more functions

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from ev3dev2.sensor.lego import Sensor, TouchSensor, UltrasonicSensor, ColorSens
 
 US = UltrasonicSensor()
 DRIVE = MoveDifferential()
+SPEED = 20
+DISTANCE = 290
 
 class Direction(IntEnum):
     NORTH = 0
@@ -18,13 +20,39 @@ class Node:
         ...
 
 def look_around() -> List[Direction]:
-    ...
+    # This function will return a list of open directions
+    # For now, we will return all directions
+    return [Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST]
 
-def move_to(neighbour: Node):
-    DRIVE.on_for_distance() # For now
+def move_forward():
+    DRIVE.on_for_distance(SPEED, DISTANCE) # For now
 
-def move_back():
-    ... # For now
+def move_backward():
+    DRIVE.on_for_distance(SPEED * -1, DISTANCE) # For now
+
+def turn_anticlockwise():
+    DRIVE.turn_degrees(SPEED * -1, 45) # For now
+
+def turn_clockwise():
+    DRIVE.turn_degrees(SPEED, 45) # For now
+
+def move_to(direction: Direction):
+    move_backward()
+    if direction == Direction.WEST:
+        turn_anticlockwise()
+    elif direction == Direction.EAST:
+        turn_clockwise()
+    move_forward()
+    DRIVE.turn_degrees(SPEED, 90) # Turn straight after moving to the new node
+    last_move = direction
+
+def move_back(last_move: Direction):
+    move_forward()
+    if last_move == Direction.WEST:
+        turn_clockwise()
+    elif last_move == Direction.EAST:
+        turn_anticlockwise()
+    move_backward()
 
 def dfs(node: Node):
     visited = set()
