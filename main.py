f31fcbf2b45e6dcce71fb613f01216843f279735
VIG0002 <vig0002@mhs.vic.edu.au>
2026-09-03 16:45:53 +1000

    Added rescue kit dispenser!!!

--- main.py
+++ main.py
@@ -1,89 +1,4 @@
 #!/usr/bin/env python3
-
-"""
-Here is exactly how to add that calibration logic to your existing functions:
-
-## 1. Add a Global Heading Variable
-At the top of your script (near your other global variables like visited), add a variable to track which absolute direction the robot chassis is currently facing:
-
-# Initialize the robot facing absolute NORTH at the start of the maze
-current_heading = Direction.NORTH  
-
-## 2. Calibrate look_around()
-Right now, look_around() assumes its relative scans match the map. You need to calibrate the relative sensor direction (d_rel) using the current_heading to find the absolute map direction (d_abs):
-
-def look_around():
-    directions = []
-
-    for d_rel in [
-        Direction.NORTH,
-        Direction.EAST,
-        Direction.WEST,
-    ]:
-        us_turn_to(d_rel)
-        if (US.distance_centimeters * 10) > MAX_WALL_DETECTION_DISTANCE:
-            # Calibrate: Add current heading to the relative sensor angle
-            # Modulo 360 keeps the degree value within the Direction enum bounds (0, 90, 180, 270)
-            d_abs = Direction((current_heading + d_rel) % 360)
-            directions.append(d_abs)
-
-    return directions
-
-## 3. Calibrate move_to()
-When dfs() passes an absolute map direction to move_to(direction), the robot needs to calculate how to move relative to its current physical heading.
-You can find the relative movement required by subtracting current_heading from the target direction. Then, update the global heading afterward:
-
-def move_to(direction):
-    global current_heading
-    
-    # Calculate the required turn relative to where the robot is facing
-    rel_move = Direction((direction - current_heading + 360) % 360)
-
-    if rel_move == Direction.NORTH:
-        move_forward()
-    elif rel_move == Direction.SOUTH:
-        move_backward()
-    elif rel_move == Direction.WEST:
-        move_backward()
-        turn_anticlockwise()
-        move_forward()
-        DRIVE.turn_degrees(SPEED, 90 - TURNING_DEGREES)
-    elif rel_move == Direction.EAST:
-        move_backward()
-        turn_clockwise()
-        move_forward()
-        DRIVE.turn_degrees(SPEED, 90 - TURNING_DEGREES)
-
-    # Calibrate: Update our absolute heading to match the new tile's orientation
-    current_heading = direction
-
-## 4. Calibrate move_back()
-When backing out of a node during the DFS backtrack phase, you must reverse this logic so the global compass heading steps backward correctly:
-
-def move_back(last_move):
-    global current_heading
-    
-    # Calculate the original relative movement that got us here
-    # (Reconstructs what 'rel_move' was inside move_to)
-    rel_move = Direction((last_move - current_heading + 360) % 360)
-
-    if rel_move != Direction.NORTH:
-        move_forward()
-    if rel_move == Direction.WEST:
-        turn_clockwise()
-    elif rel_move == Direction.EAST:
-        turn_anticlockwise()
-    if rel_move != Direction.SOUTH:
-        move_backward()
-    if rel_move == Direction.WEST or rel_move == Direction.EAST:
-        DRIVE.turn_degrees(SPEED, 90 - TURNING_DEGREES)
-
-    # Calibrate: Revert the absolute heading back to the previous tile's heading
-    # To step back, we subtract the change or compute it via the parent node's expected orientation
-    current_heading = Direction((last_move + 180) % 360) 
-
-"""
-
 from enum import IntEnum, Enum
 from ev3dev2.motor import MoveDifferential, SpeedPercent, Motor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D
 from ev3dev2.wheel import EV3Tire
@@ -107,7 +22,6 @@ class Speed(IntEnum):
     MEDIUM = 20
     FAST = 30
 
-# Copied from source
 class Color(IntEnum):
     """The different colour values returned by ev3"""
     NONE = 0
@@ -184,16 +98,20 @@ sound = Sound()
 
 class Node:
     def __init__(self):
+        '''Initialize a new node with no neighbours and no tile type.'''
         self.neighbours = {}
         self.tile_type = None
 
     def get_neighbour(self, direction):
+        '''Return the neighbour node in the given direction, or None if there is no neighbour in that direction.'''
         return self.neighbours[direction]
 
     def set_neighbour(self, direction, neighbour):
+        '''Set the neighbour node in the given direction.'''
         self.neighbours[direction] = neighbour
 
 def tile_type():
+    '''Return the type of tile the robot is currently on, based on the color sensor reading.'''
     color_value = Color(CS.color)       
     if CS.reflected_light_intensity >= REFLECTED_LIGHT_THRESHOLD:
         return TileType.START
@@ -209,6 +127,7 @@ def tile_type():
         return TileType.NORMAL # default to normal if unknown color
 
 def us_turn_to(direction):
+    '''Turn the ultrasonic sensor to face the given direction.'''
     global US_REL_DIRECTION
     assert direction != Direction.SOUTH
     assert US_REL_DIRECTION != Direction.SOUTH
@@ -228,6 +147,7 @@ def us_turn_to(direction):
     US_REL_DIRECTION = direction
     
 def look_around():
+    '''Return a list of directions that are open (no wall) from the current position.'''
     directions = []
 
     for direction in [
@@ -242,15 +162,21 @@ def look_around():
     return directions
 
 def move_forward():
+    '''Move the robot forward by one tile width, adjusting for the robot's height and the tile width. If the robot is on a start tile, it moves a different distance. If the robot is on a NOGO tile, it moves backward instead.'''
     global last_tile_was_start
     if tile_type() == TileType.START:
         DRIVE.on_for_distance(SPEED, (TILE_WIDTH - (ROBOT_HEIGHT - TILE_WIDTH / 2)))
         last_tile_was_start = True
     else:
-        DRIVE.on_for_distance(SPEED, TILE_WIDTH)
         last_tile_was_start = False
+        DRIVE.on_for_distance(SPEED, (TILE_WIDTH / 2 + 15))
+        if tile_type() != TileType.NOGO:
+            DRIVE.on_for_distance(SPEED, (TILE_WIDTH / 2 - 15))
+        else:
+            move_backward()
 
 def move_backward():
+    '''Move the robot backward by one tile width, adjusting for the robot's height and the tile width. If the last tile was a start tile, it moves a different distance.'''
     if last_tile_was_start == True:
         DRIVE.on_for_distance(SPEED * -1, (TILE_WIDTH - (ROBOT_HEIGHT - TILE_WIDTH / 2)))
     else:
@@ -263,6 +189,7 @@ def turn_clockwise():
     DRIVE.turn_degrees(SPEED, TURNING_DEGREES) # For now
         
 def move_to(direction):
+    '''Move the robot to the neighbouring node in the given direction. '''
     if direction == Direction.NORTH:
         move_forward()
     elif direction == Direction.SOUTH:
@@ -279,6 +206,7 @@ def move_to(direction):
         DRIVE.turn_degrees(SPEED, 90 - TURNING_DEGREES)
 
 def move_back(last_move):
+    '''Reverse the last move made by the robot, returning it to the previous node.'''
     if last_move != Direction.NORTH:
         move_forward()
     if last_move == Direction.WEST:
@@ -290,7 +218,12 @@ def move_back(last_move):
     if last_move == Direction.WEST or last_move == Direction.EAST:
         DRIVE.turn_degrees(SPEED, 90 - TURNING_DEGREES) # Turn straight after moving back to the previous node
 
+def dispense_rescue_kit():
+    '''Dispense a rescue kit by rotating the medium motor.'''
+    MediumMotor(OUTPUT_D).on_for_degrees(SpeedPercent(Speed.SLOW), 90)
+
 def dfs(node):
+    '''Depth-first search algorithm to explore the maze. The robot will visit each node, check for victims, and keep track of visited nodes.'''
     global visited, harmed_victim_number, unharmed_victim_number
     visited.add(node)
     open_directions = look_around()
@@ -313,6 +246,7 @@ def dfs(node):
 
             if neighbour.tile_type == TileType.HARMED_VICTIM:
                 harmed_victim_number += 1
+                dispense_rescue_kit()
                 sound.speak("Red")
                 leds.set_color("LEFT", "RED")
                 leds.set_color("RIGHT", "RED")
